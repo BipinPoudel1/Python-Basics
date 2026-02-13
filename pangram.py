6be7f7c9ad005f55d6e22c002bac48c129523ea9
Bipin <xperiabpn@gmail.com>
2026-02-13 19:49:23 +0545

    Minor change in the Pangram program

--- pangram.py
+++ pangram.py
@@ -1,10 +1,10 @@
 #Write a Python program to check whether a given string is a pangram.
-#Hint: A pangram is a sentence that contains every letter of the alphabet at least once.
+#Hint: A pangram is a sent that contains every letter of the alphabet at least once.
 
 alphabets=set( 'abcdefghijklmnopqrstuvwxyz')
-sentence= set(input("Write your sentence- ").lower().replace(" ", ""))
+sent= set(input("Write your sent- ").lower().replace(" ", ""))
 
-if alphabets.issubset(sentence):
+if alphabets.issubset(sent):
        print("Pangram")
 else:
        print("Not Pangram")
