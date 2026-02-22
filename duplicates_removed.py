d4c5105790b19f9832688031f3283f7168623b25
Bipin <xperiabpn@gmail.com>
2026-02-22 15:20:03 +0545

    Program to take a list of numbers and returns a new list with duplicates removed.

---
+++ duplicates_removed.py
@@ -0,0 +1,5 @@
+#Write a Python program that takes a list and returns a new list with duplicates removed.
+
+orgList= [6, 8, 3, 2, 6]
+duplicatesRemoved= list(set(orgList))
+print("List with duplicates removed_ ", duplicatesRemoved)
