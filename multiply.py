a8324f0d39b10053886aaa3ebcb829ced22ee348
Bipin <xperiabpn@gmail.com>
2026-03-01 12:53:20 +0545

    Function that multiplies two numbers and returns the result

---
+++ multiply.py
@@ -0,0 +1,7 @@
+#Write a function that multiplies two numbers and returns the result.
+
+def multiply(a, b):
+      result= a*b
+      return result
+
+print("Product of 2 and 3 is ", multiply(2,3))
